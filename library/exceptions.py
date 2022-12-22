960d030e5b650eb9f1ad32bca17d74091bc03d31
neoreids <neo.reids@gmail.com>
2022-12-23 05:01:57 +0800

    update wording

--- library/exceptions.py
+++ library/exceptions.py
@@ -26,7 +26,7 @@ class BookAlreadyBorrowed(APIException):
 class BookAlreadyRenewed(APIException):
     status_code = status.HTTP_400_BAD_REQUEST
     default_detail = {
-        "message": "Book is already renewed"
+        "message": "book is already renewed"
     }
 
 
